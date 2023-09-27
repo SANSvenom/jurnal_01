97163f97c684d6498c05e38b2f6c0ca1639371f2
Hasnan Alif <hasnanalif123@gmail.com>
2023-09-27 14:32:25 +0700

    Jurnal 01

---
+++ Jurnal 01 IMA.java
@@ -0,0 +1,8 @@
+class jurnal01{
+    public static void main (String[] args) {
+        System.out.println("NAMA:HASNAN ALIF");
+        System.out.println("NIM:607062300062");
+        System.out.println("KELAS:D3IF-47-01");
+    }
+
+}
